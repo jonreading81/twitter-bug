1c35270493d0fafc265b9db7be9bb9aa2858500c
Jonathan Reading <jon.reading.email@gmail.com>
2019-05-15 15:46:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.toggleDirection = this.toggleDirection.bind(this);
+    this.state = {
+      clockwise:true
+    }
+  }
+
+  toggleDirection(){
+    const {clockwise} = this.state;
+    this.setState({
+      clockwise: !clockwise
+    });
+  }
+
+  render(){
+    const {clockwise} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={`App-logo ${!clockwise && 'App-logo--back'}`} alt="logo" />
+          <div onClick={this.toggleDirection} className="App-handle"></div>
+        </header>
+      </div>
+    ); 
+  }
+  
 }
 
 export default App;
